0594fed4413493278e8c76ff930cd21e860a8f9a
chasemc67 <chasemc67@gmail.com>
2018-02-05 12:40:47 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,87 @@
+//https://www.cryptocompare.com/api/#-api-data-histominute-
+
+const https = require("https");
+
+const apiKey = "5WU911P6VS8P4472M52Q1IGYGD2BS385HT";
+const publicAddress = "0x5e8e44fe4564349925a184ef81c6459aacba2cf7";
+const url = `https://api.etherscan.io/api?module=account&action=txlist&address=${publicAddress}&startblock=0&endblock=99999999&sort=asc&apikey=${apiKey}`;
+
+var Prices = [];
+  
+https.get(url, res => {
+  res.setEncoding("utf8");
+  let body = "";
+  res.on("data", data => {
+    body += data;
+  });
+  res.on("end", () => {
+    main(JSON.parse(body));
+  });
+});
+
+
+var calculateBalanceFromTransactions = function(transactionsArray) {
+    var currentBalance = 0;
+    var transaction;
+    var gasUsedxPrice = 0;
+    for (var i=0; i < transactionsArray.length; i++) {
+        transaction = transactionsArray[i];
+        if (transaction.isError === "0"){
+            if(transaction.to === publicAddress) {
+                currentBalance += parseInt(transaction.value);
+            } else if (transaction.from === publicAddress) {
+                currentBalance -= parseInt(transaction.value);
+            }
+        }
+    }
+
+    return (currentBalance - gasUsedxPrice);
+}
+
+
+var getPriceAtTime = function (transaction, callback, lastQuery=false) {
+    const priceApi = `https://min-api.cryptocompare.com/data/pricehistorical?fsym=ETH&tsyms=BTC,USD,CAD&ts=${transaction.timeStamp}`;
+    https.get(priceApi, res => {
+        res.setEncoding("utf8");
+        let body = "";
+        res.on("data", data => {
+            body += data;
+        });
+        res.on("end", () => {
+            callback(body, transaction, lastQuery);
+        });
+    });
+}
+
+function* getPrices (transactions) {
+    console.log("starting");
+    for (var i=0; i<transactions.length; i++) {
+        getPriceAtTime(transactions[i], (response, transaction, lastQuery=false) => {
+            Prices.push({
+                timeStamp: transaction.timeStamp,
+                price: response
+            });
+
+            if (lastQuery) {
+                console.log(Prices);
+            }
+        }, i===transactions.length-1);
+        yield(sleep(1000));
+    }
+}
+
+
+var sleep = function(x) {
+    return function(cb) {
+       setTimeout(cb, x)
+    }
+ }
+ 
+
+var main = function (response) {
+    const transactions = response.result.filter((transaction) => {return transaction.isError === "0"});
+    const iterator = getPrices(transactions);
+    for (var i = 0; i < transactions.length; i++) {
+        iterator.next();
+    }
+}
