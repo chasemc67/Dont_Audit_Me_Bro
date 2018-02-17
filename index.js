46bacf0857545347f1af542cc806db4b0824a4bd
chasemc67 <chasemc67@gmail.com>
2018-02-17 00:36:58 -0800

    Looks like it works for BTC! needs testing though, and some api tuning for Eth and Bch

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ const gasPriceMultiplier = .00000001;
 
 const btcValueMultiplier = .00000001;
 
-const exchangeCurrencies = ["BTC", "USD", "CAD"];
-var currencyFlag;
+const exchangeCurrencies = ["BTC", "USD", "CAD"]; // there isn't support for changing this array yet when reading from api response;
+var chain;
 
 
 if (process.argv[2]) {
-    currencyFlag = (process.argv[2]);
+    chain = (process.argv[2].toUpperCase());
 } else {
-    currencyFlag = "btc";
+    chain = "BTC";
 }
 
 function getEthUrlForAddr(addr) {
@@ -33,31 +33,30 @@ function getBchUrlForAddr(addr) {
 }
   
 function getDataFromApi(url, callback) {
-    return new Promise(
-        function (resolve, reject) {
-            https.get(url, res => {
-                res.setEncoding("utf8");
-                let body = "";
-                res.on("data", data => {
-                    body += data;
-                });
-                res.on("end", () => {
-                    if (body.startsWith("<")) {
-                        throw Error;
-                        reject("Fail! Exceeded request limit");
-                    }
-                    resolve(JSON.parse(body));
-                });
+    return new Promise((resolve, reject) => {
+        https.get(url, res => {
+            res.setEncoding("utf8");
+            let body = "";
+            res.on("data", data => {
+                body += data;
             });
+            res.on("end", () => {
+                if (body.startsWith("<")) {
+                    throw Error;
+                    reject("Fail! Exceeded request limit");
+                }
+                resolve(JSON.parse(body));
+            });
+        });
         }
     );   
 }
 
 // Gets price for coin, in each currency in array: currencies
 // Make sure your transaction has a timestamp
-var getPriceAtTime = function (coin, currencies) {
+var getPriceAtTime = function (coin, currencies, timeStamp) {
     return new Promise((resolve, reject) => {
-        const priceApi = `https://min-api.cryptocompare.com/data/pricehistorical?fsym=${coin.toUpperCase()}&tsyms=${currencies.join().toUpperCase()}&ts=${transaction.timeStamp}`;
+        const priceApi = `https://min-api.cryptocompare.com/data/pricehistorical?fsym=${coin.toUpperCase()}&tsyms=${currencies.join().toUpperCase()}&ts=${timeStamp}`;
         https.get(priceApi, res => {
             res.setEncoding("utf8");
             let body = "";
@@ -69,6 +68,45 @@ var getPriceAtTime = function (coin, currencies) {
             });
         });
     });
+}
+
+function mapBlockChairApiResponseToTransactions(apiResponse, data) {
+    // each transaction needs to generated two apiResponses. a buy and a sell
+    
+    // TODO the bought/sold logic here is probably wrong
+
+    let apiData = apiResponse.data[0];
+    let transactions = [];
+    transactions = transactions.concat({
+        amountBought: parseInt(apiData.sum_value),
+        timeStamp: new Date(apiData.max_time_receiving).getTime() / 1000,
+        date: apiData.max_time_receiving,
+        amountSold: 0,
+        pubKey: data.pubKey
+    });
+
+
+    if (apiData.sum_value_unspent === "0") {
+        transactions = transactions.concat({
+            amountSold: parseInt(apiData.sum_value),
+            timeStamp:  new Date(apiData.max_time_spending).getTime() / 1000,
+            date: apiData.max_time_spending,
+            amountBought: 0,
+            pubKey: data.pubKey
+        });
+    } 
+
+    return transactions;
+}
+
+function mapEthApiResponseToTransactions(response, data) {
+    return({
+        txnIdHash: response.transaction.hash,
+        date: new Date(parseInt(response.transaction.timeStamp) * 1000).toString(),
+        timeStamp: response.transaction.timeStamp,
+        amountBought: (response.transaction.to === response.transaction.pubKey) ? parseInt(response.transaction.value) * ethValueMultiplier : 0,
+        amountSold: (response.transaction.from === response.transaction.pubKey) ? parseInt(response.transaction.value) * ethValueMultiplier : 0
+    })
 }
 
 
@@ -97,20 +135,18 @@ var calculateEthBalanceFromTransactions = function(transactionsArray) {
 
 function getPricedTransactions (transactions) {
     return new Promise((resolve, reject) => {
-        console.log("starting");
-        console.log(`Compiling data for ${transactions.length} transactions`);
         pricedTransactions = [];
         transactions.forEach((transaction, i, transactions) => {
             setTimeout(() => {
-                console.log(`Transaction: ${i+1}`);
-                getPriceAtTime(transaction, exchangeCurrencies).then(priceData => {
+                console.log(`Getting prices for pubKey: ${transactions[i]}, [${i+1}/${transactions.length}]`);
+                getPriceAtTime(chain, exchangeCurrencies, transaction.timeStamp).then(priceData => {
                     pricedTransactions.push(
                         Object.assign({}, 
                             transaction, 
                             {
-                                priceInBtcAtTime: response.btc,
-                                priceInUsdAtTime: response.usd,
-                                priceInCadAtTime: response.cad
+                                priceInBtcAtTime: priceData[Object.keys(priceData)[0]].BTC,
+                                priceInUsdAtTime: priceData[Object.keys(priceData)[0]].USD,
+                                priceInCadAtTime: priceData[Object.keys(priceData)[0]].CAD
                             }
                         )
                     );
@@ -123,92 +159,61 @@ function getPricedTransactions (transactions) {
     });
 }
 
-function mapBlockChairApiResponseToTransactions(responseData, data) {
-    // each transaction needs to generated two responseDatas. a buy and a sell
-    if (responseData.sum_value_unspent === "0") {
-        return ([{
-            amountBought: parseInt(responseData.sum_value),
-            timeStamp: new Date(xresponseData.max_time_receiving).getTime() / 1000,
-            amountSold: 0,
-            pubKey: data.pubKey
-        }, {
-            amountSold: parseInt(responseData.sum_value),
-            timeStamp:  new Date(responseData.max_time_spending).getTime() / 1000,
-            amountBought: 0,
-            pubKey: data.pubKey
-        }]);
-    } else {
-        return ([{
-            amountBought: parseInt(responseData.sum_value),
-            timeStamp: new Date(responseData.max_time_receiving).getTime() / 1000,
-            amountSold: 0,
-            pubKey: data.pubKey
-        }]);
+function getFinalizedTransactions (chain) {
+    var pubKeys; 
+    var apiUrlGetter;
+    var mapApiToTxns;
+
+    switch(chain) {
+        case "ETH":
+            console.log("Starting with currency ETH");
+            pubKeys = ethPubKeys;
+            apiUrlGetter = getEthUrlForAddr;
+            mapApiToTxns = mapEthApiResponseToTransactions;
+            break;
+        
+        case "BCH":
+            console.log("Starting with currency BCH");  
+            pubKeys = bchPubKeys;   
+            apiUrlGetter = getBchUrlForAddr;
+            mapApiToTxns = mapBlockChairApiResponseToTransactions;
+            break;
+        case "BTC":
+        default:
+            console.log("Starting with currency BTC");
+            pubKeys = btcPubKeys;
+            apiUrlGetter = getBtcUrlForAddr;
+            mapApiToTxns = mapBlockChairApiResponseToTransactions;
+            break;
     }
-}
 
-function mapEthToTransactions(response, data) {
-    return({
-        txnIdHash: response.transaction.hash,
-        date: new Date(parseInt(response.transaction.timeStamp) * 1000).toString(),
-        timeStamp: response.transaction.timeStamp,
-        amountBought: (response.transaction.to === response.transaction.pubKey) ? parseInt(response.transaction.value) * ethValueMultiplier : 0,
-        amountSold: (response.transaction.from === response.transaction.pubKey) ? parseInt(response.transaction.value) * ethValueMultiplier : 0,
-        priceInBtcAtTime: response.price.ETH.BTC,
-        priceInUsdAtTime: response.price.ETH.USD,
-        priceInCadAtTime: response.price.ETH.CAD
-    })
-}
-
-//Eth
-// var ethTxns = []
-// ethPubKeys.forEach((key, i, keys) => {
-//     setTimeout(() => {
-//         getDataFromApi(getEthUrlForAddr(key)).then(response => {
-//             let transactions = response.result.filter((transaction) => {return transaction.isError === "0"});
-//             transactions = transactions.map(transaction => {
-//                 transaction.pubKey = keys[i];
-//                 return transaction;
-//             });
-//             ethTxns = ethTxns.concat(response.result.filter((transaction) => {return transaction.isError === "0"}));    
-//             if (i === keys.length-1) {
-//                 getPrices(ethTxns);        
-//                 //calculateBalanceFromTransactions(ethTxns);        
-//             }
-//         }).catch(error => {
-//             console.log("Something broke");
-//             return;
-//         });
-//     }, i * 200);
-// });
-
-var btcTxns = []
-btcPubKeys.forEach((key, i, keys) => {
-    setTimeout(() => {
-        getDataFromApi(getBtcUrlForAddr(key)).then(response => {
-            // Find transaction fail flag for btc/bch
-            let addrData = response.data[0];
-            btcTxns = btcTxns.concat(mapBlockChairApiResponseToTransactions(addrData, {pubKey: keys[i]}));
-            if (i === keys.length-1) {
-                getPricedTransactions(btcTxns).then(response => {
-                    console.log(response);
-                });        
-            }
-        }).catch(error => {
-            console.log("Something broke");
-            return;
+    return new Promise((resolve, reject) => {
+        var finalTxns = []
+        pubKeys.forEach((key, i, keys) => {
+            setTimeout(() => {
+                console.log(`Getting data for pubKey: ${keys[i]}, [${i+1}/${keys.length}]`);
+                getDataFromApi(apiUrlGetter(key)).then(apiResponse => {
+                    // Find transaction fail flag for btc/bch
+                    finalTxns = finalTxns.concat(mapApiToTxns(apiResponse, {pubKey: keys[i]}));
+                    if (i === keys.length-1) {
+                        getPricedTransactions(finalTxns).then(pricedTxns => {
+                            resolve(pricedTxns);
+                        });        
+                    }
+                }).catch(error => {
+                    console.log("Something broke");
+                    return;
+                });
+            }, i * 200);
         });
-    }, i * 200);
-});
+    });
+}
 
 
-// const key = ethPubKeys[5]
-// getDataFromApi(getEthUrlForAddr(key)).then(response => {
-//     let transactions = response.result.filter((transaction) => {return transaction.isError === "0"});
-//     transactions = transactions.map(transaction => {
-//         transaction.pubKey = key;
-//         return transaction;
-//     });
-//     ethereumTransactions = ethereumTransactions.concat(response.result.filter((transaction) => {return transaction.isError === "0"}));    
-//     calculateBalanceFromTransactions(ethereumTransactions);        
-// });
+function main() {
+    getFinalizedTransactions(chain).then(finalizedTxns => {
+        console.log(finalizedTxns);
+    });
+}
+
+main();
